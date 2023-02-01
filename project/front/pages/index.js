ce544999197608061184f3ceda0324ad96d6e4be
yjglab <yjgdesign@gmail.com>
2023-02-02 01:10:24 +0900

    Add: Navigation css

--- project/front/pages/index.js
+++ project/front/pages/index.js
@@ -1,12 +1,11 @@
 import React from "react";
 import AppLayout from "../components/AppLayout";
+import Navigation from "../components/Navigation";
 
 const Home = () => {
   return (
     <AppLayout>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
+      <Navigation />
     </AppLayout>
   );
 };
