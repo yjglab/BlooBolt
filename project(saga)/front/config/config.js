9e2b271edb460714e6203a3c97b20889651dbb40
yjglab <yjgdesign@gmail.com>
2023-02-27 20:36:50 +0900

    .

--- project(saga)/front/config/config.js
+++
@@ -1,4 +0,0 @@
-export const backUrl =
-  process.env.NODE_ENV === "production"
-    ? "http://bloobolt.com"
-    : "http://localhost:4080";
--- project(saga)/front/hooks/useInput.js
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from "react";
-
-export default (initialValue = null) => {
-  const [value, setValue] = useState(initialValue);
-  const handler = useCallback((e) => {
-    setValue(e.target.value);
-  }, []);
-  return [value, handler, setValue];
-};
